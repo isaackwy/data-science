c43a82fd10529eb51a222e842a6d96cc7df594fe
Isaac <isaacyu1@isaacyu1.com>
2019-07-03 17:28:52 -0700

    Updating loadTweets.R

--- USpolitics1/loadTweets.R
+++ USpolitics1/loadTweets.R
@@ -3,7 +3,7 @@ library(rtweet)
 # Connect to API. Keys not uploaded to github.
 source('connect_to_twitter.R')
 
-Drumpf2019 <- get_timeline("realDonaldTrump", n = 3200)
-Birdie2019 <- get_timeline("BernieSanders", n = 3200)
-save(Drumpf2019, file="Drumpf2019.RData")
-save(Birdie2019, file="Birdie2019.RData")
+Trump2019 <- get_timeline("realDonaldTrump", n = 3200)
+Sanders2019 <- get_timeline("BernieSanders", n = 3200)
+save(Trump2019, file="Trump2019.RData")
+save(Sanders2019, file="Sanders2019.RData")
